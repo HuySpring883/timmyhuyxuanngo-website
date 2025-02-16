5d013f10e2b43c418ce79658c64e0674b8ffa31d
Timmy Huy Xuan Ngo <tim.huyxuan.ngo@gmail.com>
2025-02-16 12:44:53 -0500

    Delete Portfolio-2.0 directory

--- Portfolio-2.0/script.js
+++
@@ -1,9 +0,0 @@
-/* Live Time Function */
-function displayTime() {
-    let currentTime = new Date(); 
-    let formattedTime = currentTime.toLocaleTimeString();
-    document.getElementById("current-time").textContent = formattedTime;
-}
-
-displayTime(); 
-setInterval(displayTime, 1000); // Update every second
