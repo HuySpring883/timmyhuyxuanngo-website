7ef7729eb3bbd8b3a8a0267a72703ca4187a216a
Huy <tim.huyxuan.ngo@gmail.com>
2025-06-03 21:45:58 -0400

    content update

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 /* Live Time Function */
 function displayTime() {
     let currentTime = new Date(); 
-    let formattedTime = currentTime.toLocaleTimeString();
+    let formattedTime = currentTime.toLocaleTimeString([], {
+        hour: '2-digit',
+        minute: '2-digit'
+    });
     document.getElementById("current-time").textContent = formattedTime;
 }
 
